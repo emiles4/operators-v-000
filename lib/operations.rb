52f0d7821d1e416ff2d12c4c16df23856ff3d719
Emily Miles <emiles4@gmail.com>
2018-09-28 04:08:02 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,6 +1,12 @@
 def unsafe?(speed)
-if speed.between?(30,60)
-	return true
+	if speed>60
+		return true
+		if speed <40
+			return true
+if speed.between?(40,60)
+	return false
+end
+end
 end
 end
 
